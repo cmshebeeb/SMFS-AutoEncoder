6979e82fa0b643d5c4e2ec894d920ff3db07d077
cmshebeeb <mshebeebc@gmail.com>
2026-08-06 13:11:46 +0530

    Baseline Autoencoder class implemented

---
+++ src/models/autoencoder.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class AutoEncoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU()
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(hidden_dim, input_dim)
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
