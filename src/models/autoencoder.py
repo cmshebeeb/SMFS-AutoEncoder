185d184c0d5d9e4505ba69670353b73acdab336b
cmshebeeb <mshebeebc@gmail.com>
2026-08-12 10:41:41 +0530

    updated autoencoder.py

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -2,11 +2,14 @@ import torch
 import torch.nn as nn
 
 class AutoEncoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, lambda_l21=0.001):
+        self.lambda_l21 = lambda_l21
         super().__init__()
 
+        self.encoder_layer = nn.Linear(input_dim, hidden_dim)
+
         self.encoder = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
+            self.encoder_layer,
             nn.ReLU()
         )
 
@@ -18,3 +21,7 @@ class AutoEncoder(nn.Module):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
+    
+    def l21_penalty(self):
+        weights = self.encoder_layer.weight
+        return torch.norm(weights, dim=0).sum()
